c3dea8ea0c9bd6f55765874816ee4e7f7072954f
Vihal Karhade <130913527+VihalKarhade@users.noreply.github.com>
2024-01-22 10:28:20 +0530

    Create O(n).py

---
+++ Big-O/O(n).py
@@ -0,0 +1,10 @@
+# in grapf form O(n) is always a straight line i.e. proportional
+
+def print_items(n):
+    for i in range (n):
+        print(i)
+
+print_items(10)
+
+
+# O(log n) is the Devide and Conquer
